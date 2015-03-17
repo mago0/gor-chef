df2e31069c773e0d9aaba521f4dbffc8c6235460
Matt Williams <matt@tilt.com>
2015-03-17 10:35:49 -0400

    No templates, just install

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,14 +3,14 @@
 # Attributes:: default
 #
 # Author:: Wouter de Vos <wouter@springest.com>, Peter de Ruijter <edge@springest.com>
+# Modified By:: Matt Williams <matt@williams-tech.net>
 #
 # Copyright 2012-2013, Wouter de Vos
 #
 
 include_recipe "golang::default"
 
-
-git "~/gor" do
+git "/usr/local/src/gor" do
   repository node['gor']['distribution']['repository']
   reference node['gor']['distribution']['revision']
   action :sync
@@ -18,32 +18,16 @@ git "~/gor" do
   not_if do ::File.exists?("/usr/local/bin/gor") end
 end
 
-
 execute "compile-gor-source" do
-  cwd "~/gor"
+  cwd "/usr/local/src/gor"
   command "go build gor.go"
   action :nothing
   notifies :run, 'execute[move-bin-to-folder]', :immediately
 end
 
 execute "move-bin-to-folder" do
-  cwd "~/gor"
+  cwd "/usr/local/src/gor"
   command "mv ./gor /usr/local/bin/gor"
   action :nothing
 end
 
-template '/usr/local/bin/gor_listen' do
-  source 'gor_do.erb'
-  owner 'root'
-  mode 0744
-  action :create
-  only_if( node['gor']['role'] == 'listen' )
-end
-
-template '/usr/local/bin/gor_play' do
-  source 'gor_do.erb'
-  owner 'root'
-  mode 0744
-  action :create
-  only_if( node['gor']['role'] == 'play' )
-end
